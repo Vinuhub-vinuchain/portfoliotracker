e1fadc97afbcf61b35e14678ef3db844c367315c
VINUHUB <aladoemeka1@gmail.com>
2025-11-17 18:02:57 -0800

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+// vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  server: {
+    port: 5173,
+    open: true,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
+})
